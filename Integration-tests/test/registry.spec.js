24a06e7625a9eb34ea75c10d7d599b96e391b320
yakovlevMQA <yakovlevmqa@gmail.com>
2023-10-08 19:03:48 +0400

    CAV-16: fixed tests and refactor

--- Integration-tests/test/registry.spec.js
+++ Integration-tests/test/registry.spec.js
@@ -1,205 +1,174 @@
 const {test, expect} = require('@playwright/test');
+const {getUserByEmail, insertUser} = require('../main/db-utils');
+const uuid = require('uuid');
+
 test.describe.parallel("Registration testing", () => {
-    const baseUrl = 'http://localhost:8080'
-    test('POST - create new renter', async ({request}) => {
+    const baseUrl = 'http://localhost:8080';
+
+    test(`POST - create new user with role renter`, async ({request}) => {
+        const userData = {
+            name: 'Seva', login: 'seva@gmail.com', password: '1q2w!aA123', role: 'renter',
+        };
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
-            data: {
-                name: 'Seva',
-                email: 'seva@gmail.com',
-                password: '1q2w!aA123',
-                role: 'renter',
-            },
+            data: userData
         });
-        expect(response.status()).toBe(200)
+
+        expect(response.status()).toBe(200);
+
+        const users = await getUserByEmail(userData.login);
+        expect(users.length).toBe(1);
+        expect(users[0].Name).toBe(userData.name);
+        expect(users[0].Role).toBe(userData.role);
     });
-    test('POST - create new client', async ({request}) => {
+    test(`POST - create new user with role client`, async ({request}) => {
+        const userData = {
+            name: 'Max', login: 'max@gmail.com', password: '1q2w!aA123', role: 'client'
+        };
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
-            data: {
-                name: 'Max',
-                email: 'max@gmail.com',
-                password: '1q2w!aA123',
-                role: 'client',
-            },
+            data: userData
         });
-        expect(response.status()).toBe(200)
+
+        expect(response.status()).toBe(200);
+
+        const users = await getUserByEmail(userData.login);
+        expect(users.length).toBe(1);
+        expect(users[0].Name).toBe(userData.name);
+        expect(users[0].Role).toBe(userData.role);
     });
+
     test('POST - create same email user', async ({request}) => {
+        const existingUserData = {
+            userId: uuid.v4(), name: 'Max', login: 'max2@gmail.com', password: '1q2w!aA123', role: 'renter',
+        };
+        const newUserWithSameEmail = {
+            name: 'Fake Max', login: 'max2@gmail.com', password: '1q2w!aA123', role: 'renter',
+        };
+        await insertUser(existingUserData);
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
-            data: {
-                name: 'Max',
-                email: 'seva@gmail.com',
-                password: '1q2w!aA123',
-                role: 'rentor',
-            },
+            data: newUserWithSameEmail,
         });
-        //const responseBody = JSON.parse(await response.text())
-        //console.log(responseBody)
-        expect(response.status()).toBe(400)
-        //expect(responseBody[0].message).toBe("The Password must contain at least 8 characters")
+        expect(response.status()).toBe(409);
+        const responseBody = JSON.parse(await response.text());
+        expect(responseBody[0].code).toBe("UserAlreadyExist");
     });
     test('POST - create user with short password', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva@gmail.com',
-                password: '1q2w!aA',
-                role: 'renter'
-            },
+                name: 'Seva', login: 'seva@gmail.com', password: '1q2w!aA', role: 'renter'
+            }
         });
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Password must contain at least 8 characters")
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("PasswordTooShort");
     });
     test('POST - password without capital letters', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva@gmail.com',
-                password: '1q2w!aa223',
-                role: 'renter',
+                name: 'Seva', login: 'seva@gmail.com', password: '1q2w!aa223', role: 'renter',
             },
         });
-         const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Password must contain at least one capital letter")
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("PasswordMissingUppercase");
     });
     test('POST - password without specsymbols', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva@gmail.com',
-                password: '1q2Waa223',
-                role: 'renter',
+                name: 'Seva', login: 'seva@gmail.com', password: '1q2Waa223', role: 'renter',
             },
         });
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Password must contain at least one special character")
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("PasswordMissingSpecialCharacter");
     });
     test('POST - password without digits', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva@gmail.com',
-                password: 'aqewaaFf!',
-                role: 'renter',
+                name: 'Seva', login: 'seva@gmail.com', password: 'aqewaaFf!', role: 'renter',
             },
-        })
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Password must contain at least one digit")
+        });
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("PasswordMissingDigit");
     });
     test('POST - invalid email format', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva@gmail',
-                password: '1q2w!aA123',
-                role: 'renter',
+                name: 'Seva', login: 'seva@gmail', password: '1q2w!aA123', role: 'renter',
             },
-        })
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Email format is not valid")
+        });
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("InvalidEmailFormat");
     });
     test('POST - invalid email format - 2', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva@',
-                password: '1q2w!aA123',
-                role: 'renter',
-            },
-        })
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Email format is not valid")
+                name: 'Seva', login: 'seva@', password: '1q2w!aA123', role: 'renter',
+            }
+        });
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("InvalidEmailFormat");
     });
     test('POST - invalid email format - 3', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva_gmail.com',
-                password: '1q2w!aA123',
-                role: 'renter',
-            },
+                name: 'Seva', login: 'seva_gmail.com', password: '1q2w!aA123', role: 'renter',
+            }
         })
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Email format is not valid")
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("InvalidEmailFormat");
     });
     test('POST - invalid user role', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
-                name: 'Seva',
-                email: 'seva2@gmail.com',
-                password: '1q2w!aA123',
-                role: 'rentor'
+                name: 'Seva', login: 'seva2@gmail.com', password: '1q2w!aA123', role: 'rentor'
             }
         });
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Role is not valid")
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("InvalidRole");
     })
     test('POST - too long name', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
                 name: 'Seva12345123451234512345123451234512345123451234512',
-                email: 'seva2@gmail.com',
+                login: 'seva2@gmail.com',
                 password: '1q2w!aA123',
                 role: 'renter',
-            },
+            }
         })
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Name is longer than 50 characters")
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("NameIsTooLong");
     });
     test('POST - too long email', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
                 name: 'Seva',
-                email: 'seva12345123451234512345123451234512345123451234512@gmail.com',
+                login: 'seva12345123451234512345123451234512345123451234512@gmail.com',
                 password: '1q2w!aA123',
                 role: 'renter',
-            },
+            }
         })
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Email is longer than 50 characters")
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("EmailIsTooLong");
     });
     test('POST - too long password', async ({request}) => {
         const response = await request.post(`${baseUrl}/api/v1/registry`, {
             data: {
                 name: 'Seva',
-                email: 'seva@gmail.com',
+                login: 'seva@gmail.com',
                 password: '1q2w!aA1231q2w!aA1231q2w!aA1231q2w!aA1231q2w!aA1231',
                 role: 'renter',
-            },
-        })
-        const responseBody = JSON.parse(await response.text())
-        expect(response.status()).toBe(400)
-        expect(responseBody[0].description).toBe("The Password is longer than 50 characters")
+            }
+        });
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(400);
+        expect(responseBody[0].code).toBe("PasswordIsTooLong");
     });
 });
 
-// import {Client} from "pg";
-//
-//
-// const client = new Client({
-//     host: "localhost",
-//     user: "postgres",
-//     port: "5432:5432",
-//     password: "postgres",
-//     database: "sso-postgres"
-// })
-
-// client.connect();
-//
-// client.query('Select * from "Users"', (err, res) => {
-//     if (!err) {
-//         console.log(res.rows);
-//     } else {
-//         console.log(err.message);
-//     }
-// })
-// client.end;
