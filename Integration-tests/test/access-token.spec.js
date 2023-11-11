1d468b209738b02d14099dec5996755541c25358
yakovlevMQA <yakovlevmqa@gmail.com>
2023-11-11 21:05:26 +0400

    CAV-25: mock-server+allure example

--- Integration-tests/test/access-token.spec.js
+++ Integration-tests/test/access-token.spec.js
@@ -3,9 +3,10 @@ const {allure} = require("allure-playwright")
 test.describe.parallel("Access token testing", () => {
 
     test(`GET - get token info`, async ({request}) => {
+        let response;
         await allure.step("Visit todolist page", async () => {
-            const response = await request.get(`http://localhost:3001/api/v1/mock-token`);
+            response = await request.get(`http://localhost:3001/api/v1/mock-token`);
         });
-        //expect(response.status()).toBe(200);
+        expect(response.status()).toBe(200);
     });
 });
