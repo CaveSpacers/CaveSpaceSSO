4ac48269ffdfac5e80200f0c31f03724e8f76b27
yakovlevMQA <yakovlevmqa@gmail.com>
2023-11-12 12:42:42 +0400

    CAV-25: mock env fix

--- Integration-tests/config.js
+++ Integration-tests/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     dbhost: process.env.DB_HOST ?? '5432:5432',
     baseUrl: process.env.BASE_URL ?? 'http://localhost:8080',
+    mockBaseUrl: process.env.MOCK_BASE_URL ?? 'http://localhost:3001',
 };
--- Integration-tests/test/access-token.spec.js
+++ Integration-tests/test/access-token.spec.js
@@ -1,11 +1,12 @@
 const {test, expect} = require('@playwright/test');
 const {allure} = require("allure-playwright")
+const {mockBaseUrl} = require("../config");
 test.describe.parallel("Access token testing", () => {
 
     test(`GET - get token info`, async ({request}) => {
         let response;
         await allure.step("Visit todolist page", async () => {
-            response = await request.get(`http://localhost:3001/api/v1/mock-token`);
+            response = await request.get(`http://wiremock:3001/api/v1/mock-token`);
         });
         expect(response.status()).toBe(200);
     });
