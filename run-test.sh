dbb1c293797e1446d91473f36646126777dc5349
yakovlevMQA <yakovlevmqa@gmail.com>
2023-12-09 23:39:35 +0400

    CAV-30: fix pipeline-5

--- run-test.sh
+++ run-test.sh
@@ -3,3 +3,7 @@ docker-compose -f docker-compose-local.yml --profile tests build &&
 docker-compose -f docker-compose-local.yml --profile tests up
 test_status=$?
 exit $test_status
+      if [ $test_status -ne 0 ]; then
+          echo "Tests failed. Exiting with status $test_status."
+          exit $test_status
+      fi
