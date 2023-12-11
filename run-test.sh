99e335e36e77ca631f1723d09a57830587ca215b
vse-pike <vse.pike@proton.me>
2023-12-11 19:25:31 +0600

    Debug 6

--- run-test.sh
+++ run-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker-compose -f docker-compose-local.yml --profile tests build &&
 docker-compose -f docker-compose-local.yml --profile tests up --exit-code-from=sso-integration-tests
-exit_code=$?
+#exit_code=$?
 
 #echo $exit_code
 #
-#exit $exit_code
+exit $?
 #test_status=$?
 #exit $test_status
 #      if [ $test_status -ne 0 ]; then
